65f359def9251e42bfc86c53b2c7c02166e8f98d
Oscar Ryley <oscar.ryley@gmail.com>
2023-11-25 23:41:14 +0000

    Sign now shows small version of the previous shape
    Using a similar spawning method to the target grid, the sign on the robotic arm now displays the grid pattern build in the previous level

--- Global.gd
+++ Global.gd
@@ -13,6 +13,13 @@ var total = 1000
 var two_in_three = [0, 1, 1]
 @export var spawn_object_block = preload("res://block.tscn")
 @export var spawn_object_square = preload("res://target_square.tscn")
+@export var spawn_object_sign_square = preload("res://sign_square.tscn")
+var previous_target_grid = [[0, 0, 0, 0, 0, 0], 
+							[0, 0, 0, 0, 0, 0],
+							[0, 0, 0, 0, 0, 0], 
+							[0, 0, 0, 0, 0, 0], 
+							[0, 0, 0, 0, 0, 0], 
+							[0, 0, 0, 0, 0, 0] ]
 
 func new_target():
 	total = 0
@@ -44,7 +51,9 @@ func new_target():
 		for item in row:
 			if item == 1:
 				total += 1
+	spawn_sign_squares()
 	spawn_squares()
+	previous_target_grid = target_grid
 
 func _ready():
 	new_target()
@@ -83,6 +92,30 @@ func spawn_squares():
 				var obj = spawn_object_square.instantiate()
 				obj.position = locations[i][j]
 				square_array.append(obj)
+				add_child(obj)
+			index += 1
+
+var sign_square_array = []
+
+var sdiff = -20
+var sign_locations = [[Vector2(5*sdiff,5*sdiff), Vector2(4*sdiff,5*sdiff), Vector2(3*sdiff,5*sdiff), Vector2(2*sdiff,5*sdiff), Vector2(1*sdiff,5*sdiff), Vector2(0*sdiff,5*sdiff)], 
+				[Vector2(5*sdiff,4*sdiff), Vector2(4*sdiff,4*sdiff), Vector2(3*sdiff,4*sdiff), Vector2(2*sdiff,4*sdiff), Vector2(1*sdiff,4*sdiff), Vector2(0*sdiff,4*sdiff)],
+				[Vector2(5*sdiff,3*sdiff), Vector2(4*sdiff,3*sdiff), Vector2(3*sdiff,3*sdiff), Vector2(2*sdiff,3*sdiff), Vector2(1*sdiff,3*sdiff), Vector2(0*sdiff,3*sdiff)], 
+				[Vector2(5*sdiff,2*sdiff), Vector2(4*sdiff,2*sdiff), Vector2(3*sdiff,2*sdiff), Vector2(2*sdiff,2*sdiff), Vector2(1*sdiff,2*sdiff), Vector2(0*sdiff,2*sdiff)], 
+				[Vector2(5*sdiff,1*sdiff), Vector2(4*sdiff,1*sdiff), Vector2(3*sdiff,1*sdiff), Vector2(2*sdiff,1*sdiff), Vector2(1*sdiff,1*sdiff), Vector2(0*sdiff,1*sdiff)],
+				[Vector2(5*sdiff, 0*sdiff), Vector2(4*sdiff, 0*sdiff), Vector2(3*sdiff, 0*sdiff), Vector2(2*sdiff, 0*sdiff), Vector2(1*sdiff, 0*sdiff), Vector2(0*sdiff, 0*sdiff)] ]
+
+func spawn_sign_squares():
+	for i in sign_square_array:
+		i.queue_free()
+	sign_square_array = []
+	var index = 0
+	for i in range(0,6):
+		for j in range(0,6):
+			if previous_target_grid[i][j] == 1:
+				var obj = spawn_object_sign_square.instantiate()
+				obj.position = sign_locations[i][j] + Vector2(174, 820)
+				sign_square_array.append(obj)
 				add_child(obj)
 			index += 1
 
